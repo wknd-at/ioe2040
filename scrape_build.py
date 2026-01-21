d1d15054a273ab43ec41fb346cf2a858d6d226ab
wknd-at <software-licensing@weekendonline.at>
2026-01-21 14:02:45 +0100

    Update scrape_build.py

--- scrape_build.py
+++ scrape_build.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
-SOURCE_URL = "https://www.initiativeoesterreich2040.at/unsere-unterstuetzer"
+SOURCE_URL = "https://www.initiativeoesterreich2040.at/unsere-unterstuetzer-build"
 BASE = "https://www.initiativeoesterreich2040.at"
 
 OUT_DIR = "dist"
